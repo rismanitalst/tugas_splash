335195e103ebfc129fd2c2444a5806e8d91b9bf7
rismanitalst <rismanitalst@gmail.com>
2025-11-02 20:34:47 +0700

    setup base layout for splash screen

---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // content coming soon
+          ],
+        ),
+      ),
+    );
+  }
+}
