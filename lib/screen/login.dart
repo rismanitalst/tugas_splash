1c80a0f0e74511b61d8c76f70a18360130282fed
rismanitalst <rismanitalst@gmail.com>
2025-11-02 23:46:48 +0700

    add lock icon n text login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,14 +6,48 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              SizedBox(height: 80),
+            crossAxisAlignment: CrossAxisAlignment.start, // biar ke kiri
+            children: [
+              const SizedBox(height: 80),
+
+              // Icon biru dengan ikon kunci
+              Container(
+                width: 100,
+                height: 100,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF00008B),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.lock_outline,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Teks masih di kiri
+              const Text(
+                "Welcome ",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                "Silakan login untuk melanjutkan",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.blueGrey,
+                ),
+              ),
             ],
           ),
         ),
