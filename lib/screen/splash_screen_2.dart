ac7d4e4498ca5b2e05150a5997c5da2393b0c4b6
rismanitalst <rismanitalst@gmail.com>
2025-11-02 22:37:07 +0700

    add image, text, 3 dots, n continue button to splash screen 2

--- lib/screen/splash_screen_2.dart
+++ lib/screen/splash_screen_2.dart
@@ -1,12 +1,125 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:tugas_splash/screen/splash_screen_3.dart';
 
-  class SplashScreen2 extends StatelessWidget {
-    const SplashScreen2({super.key});
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return const Scaffold(
-        body: Center(child: Text("Splash Screen 2")),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+
+            // Gambar bulat
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFF00008B),
+                image: const DecorationImage(
+                  image: AssetImage("assets/images/splash2.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 28),
+
+            // Judul
+            const Text(
+              "Stay Connected 🌐",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Subjudul
+            const Text(
+              "Your payments, your control.\nAnytime. Anywhere 💡",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Indikator aktif di posisi kedua
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFD9D9D9),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF00008B),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFD9D9D9),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 30),
+
+            // Tombol Continue ke SplashScreen3
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: SizedBox(
+                height: 42,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SplashScreen3(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF00008B),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
